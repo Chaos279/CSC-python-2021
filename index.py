65962886cfd04f7b5c9cac8309f4c2fd53180c79
Chaos279 <benedickcruz1003@gmail.com>
2021-09-29 22:23:55 +1300

    Added Working Print Button and added errors
    Added a working print button that displays the appended data. Also added error messages when for data type errors when typing in the data.

--- index.py
+++ index.py
@@ -7,15 +7,31 @@ from tkinter import *
 root=Tk()
 
 #global variables used
-global item_details, entry1, entry2, entry3, entry4, entry5, total_entries
+global item_details, entry1, entry2, entry3, entry4, entry5, total_entries, row_count
 total_entries = 0
 item_details = []
-
+row_count = 0
 
 #Button commands
 
+def print(): #print camp details
+    global item_details, entry1, entry2, entry3, entry4, entry5, total_entries, row_count
+    while row_count < total_entries :
+            Label(root, text=row_count).grid(column=0,row=row_count+9) 
+            Label(root, text=(item_details[row_count][0])).grid(column=1,row=row_count+9)
+            Label(root, text=(item_details[row_count][1])).grid(column=2,row=row_count+9)
+            Label(root, text=(item_details[row_count][2])).grid(column=3,row=row_count+9)
+            Label(root, text=(item_details[row_count][3])).grid(column=4,row=row_count+9)
+            row_count +=  1
+
+
+    
+
 def quit(): #code for button3 to leave code
     root.destroy()
+
+
+
 
 
  
@@ -27,29 +43,43 @@ def check(data, data_type): #code used to check if the entered value is an integ
         return False            
 
 def append():  # add next item to the list
-    global item_details, entry1, entry2, entry3, entry4, entry5, total_entries
+    global item_details, entry1, entry2, entry3, entry4, entry5, total_entries, row_count, error1, error2
     #global variables used
-    if len(entry1.get()) !=0 and len(entry2.get()) !=0 and len(entry3.get()) !=0 and len(entry4.get()) !=0 and check(entry2.get(), int) and check(entry4.get(), int) <= 500 and check(entry4.get(), int) >= 1:
+    if len(entry1.get()) !=1 and len(entry2.get()) !=1 and len(entry3.get()) !=1 and len(entry4.get()) !=1 and check(entry2.get(), int) and check(entry4.get(), int):
 
-        item_details.append([entry1.get(),entry2.get(),entry3.get(),entry4.get()])
-        print("Hello World")
-
+        if 1 <= int(entry4.get()) <= 500:
         
-        entry1.delete(0,'end')
-        entry2.delete(0,'end')
-        entry3.delete(0,'end')
-        entry4.delete(0,'end')
-        total_entries +=  1
+            item_details.append([entry1.get(),entry2.get(),entry3.get(),entry4.get()])
+            entry1.delete(0,'end')
+            entry2.delete(0,'end')
+            entry3.delete(0,'end')
+            entry4.delete(0,'end')
+            total_entries +=  1
 
-    elif check(entry4.get(), int):
+            for obj in root.grid_slaves(row=4, column=4):
+                obj.destroy()
 
-        if int(entry4.get()) =< 1 and int(entry4.get()) >= 500:
+            for obj in root.grid_slaves(row=2, column=4):
+                obj.destroy()
 
-            Label(root, text="Enter a number between 1 and 500", fg="red").grid(column=4, row=4)
+        else:
+            for obj in root.grid_slaves(row=4, column=4):
+                obj.destroy()
+            for obj in root.grid_slaves(row=2, column=4):
+                obj.destroy()
+                
+            error1 = Label(root, text="Enter a number between 1 and 500", fg="red").grid(column=4, row=4)
+            
 
     else :
 
-        Label(root, text="Only Numbers", fg="red").grid(column=4, row=2)
+        for obj in root.grid_slaves(row=4, column=4):
+                obj.destroy()
+        for obj in root.grid_slaves(row=2, column=4):
+                obj.destroy()
+                
+        error2 = Label(root, text="Only Numbers", fg="red").grid(column=4, row=2)
+        error1 = Label(root, text="Only Numbers", fg="red").grid(column=4, row=4)
 
 
     
@@ -77,9 +107,9 @@ entry3 = Entry(root, width="40")
 entry4 = Entry(root, width="40")
 entry5 = Entry(root, width="40")
 
-#Buttons labeled here
+#Buttons placed here
 button1 = Button(root, text="Append", padx = 30, pady = 10, command = append)
-button2 = Button(root, text="Print", padx = 30, pady = 10)
+button2 = Button(root, text="Print", padx = 30, pady = 10, command = print)
 button3 = Button(root, text="Delete", padx = 20, pady = 10)
 button4 = Button(root, text="Exit", padx = 30, pady = 10, command = quit)
 
@@ -102,14 +132,14 @@ label9.grid(column=2, row=8)
 label10.grid(column=3, row=8)
 label0.grid(column=4, row=8)
 
-# Entry field positions
+# Entry field placed on the grid
 entry1.grid(column=1, row=1, columnspan=3)
 entry2.grid(column=1, row=2, columnspan=3)
 entry3.grid(column=1, row=3, columnspan=3)
 entry4.grid(column=1, row=4, columnspan=3)
 entry5.grid(column=1, row=5, columnspan=3)
 
-#Button positions
+#Button placed on the grid
 button1.grid(column=0, row=7)
 button2.grid(column=1, row=7)
 button3.grid(column=2, row=7)
